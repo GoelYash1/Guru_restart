c2dbdab4486b6c5fe9ccc6c0df9d66e7ba9baf22
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2023-04-10 03:06:36 +0530

    Work on the UI has started, remade the Onboarding, Setting and Attendance Part

--- app/src/main/java/com/axyz/upasthithguru/activity/EntryActivity.kt
+++ app/src/main/java/com/axyz/upasthithguru/activity/EntryActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import androidx.lifecycle.Observer
 import com.axyz.upasthithguru.R
 import com.axyz.upasthithguru.app
+import com.axyz.upasthithguru.databinding.ActivityEntryBinding
 
 import io.realm.kotlin.mongodb.User
 import okhttp3.internal.wait
@@ -16,20 +17,26 @@ import okhttp3.internal.wait
 
 
 class EntryActivity : AppCompatActivity() {
-    //    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityEntryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityEntryBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_entry)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-        val btn_register = findViewById<Button>(R.id.btn_register)
-        btn_register.setOnClickListener {
-            startActivity(Intent(applicationContext, RegisterActivity::class.java))
+        setContentView(binding.root)
+        val btn_new_journey = findViewById<Button>(R.id.btn_new_journey)
+        btn_new_journey.setOnClickListener {
+            navigateToRegisterActivity()
         }
 
-        val btn_login = findViewById<Button>(R.id.btn_login)
-        btn_login.setOnClickListener {
-            startActivity(Intent(applicationContext, LoginActivity::class.java))
+        val btn_continue_journey = findViewById<Button>(R.id.btn_continue_journey)
+        btn_continue_journey.setOnClickListener {
+            navigateToLoginActivity()
         }
+    }
+
+    private fun navigateToLoginActivity(){
+        startActivity(Intent(applicationContext, LoginActivity::class.java))
+    }
+    private fun navigateToRegisterActivity(){
+        startActivity(Intent(applicationContext, RegisterActivity::class.java))
     }
 }
--- app/src/main/java/com/axyz/upasthithguru/activity/OnboardingActivity.kt
+++ app/src/main/java/com/axyz/upasthithguru/activity/OnboardingActivity.kt
@@ -1,27 +1,29 @@
 package com.axyz.upasthithguru.activity
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.axyz.upasthithguru.R
+import com.axyz.upasthithguru.databinding.ActivityMainBinding
 import com.axyz.upasthithguru.other.OnboardingItem
 import com.axyz.upasthithguru.other.OnboardingItemsAdapter
-import com.google.android.material.button.MaterialButton
 
 class OnboardingActivity : AppCompatActivity() {
 
     private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
+    private lateinit var binding: ActivityMainBinding
     private lateinit var indicatorsContainer: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setOnboardingItems()
         setupIndicators()
         setCurrentIndicator(0)
@@ -31,24 +33,27 @@ class OnboardingActivity : AppCompatActivity() {
         onboardingItemsAdapter = OnboardingItemsAdapter(
             listOf(
                 OnboardingItem(
-                    onboardingImage = R.drawable.task,
-                    title = "Manage your Task",
-                    description = "Organize all your to do's and projects. Color tag them to set priorities and categories."
+                    onBgImage = R.drawable.onbg1,
+                    onboardingImage = R.drawable.onclassroom,
+                    title = "Easy Attendance Marking System",
+                    description = "Mark the attendance of your class via bluetooth within just seconds"
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.time,
-                    title = "Work on Time",
-                    description = "When you're overwhelmed by the amount of work you have on your plate, stop and rethink."
+                    onBgImage = R.drawable.onbg2,
+                    onboardingImage = R.drawable.onreport,
+                    title = "Track Your Students' Attendance",
+                    description = "Keep track of your students' attendance with a trouble-free data system"
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.reminder,
-                    title = "Get Reminder on Time",
-                    description = "When you encounter a small task that takes less than 5 minutes to complete."
+                    onBgImage = R.drawable.onbg3,
+                    onboardingImage = R.drawable.onschedule,
+                    title = "Manage Your Lectures' Schedule",
+                    description = "Schedule and manage all your lectures effortlessly"
                 )
 
             )
         )
-        val onboardingViewPager = findViewById<ViewPager2>(R.id.onboardingViewPager)
+        val onboardingViewPager:ViewPager2 = binding.onboardingViewPager
         onboardingViewPager.adapter = onboardingItemsAdapter
         onboardingViewPager.registerOnPageChangeCallback(object:
             ViewPager2.OnPageChangeCallback(){
@@ -59,7 +64,7 @@ class OnboardingActivity : AppCompatActivity() {
         })
         (onboardingViewPager.getChildAt(0) as RecyclerView).overScrollMode =
             RecyclerView.OVER_SCROLL_NEVER
-        findViewById<ImageView>(R.id.image_next).setOnClickListener {
+        binding.imageNext.setOnClickListener {
             if(onboardingViewPager.currentItem+1<onboardingItemsAdapter.itemCount){
                 onboardingViewPager.currentItem+=1
             }
@@ -67,25 +72,23 @@ class OnboardingActivity : AppCompatActivity() {
                 navigateToHomeActivity()
             }
         }
-        findViewById<TextView>(R.id.textSkip).setOnClickListener {
-            navigateToHomeActivity()
-        }
-        findViewById<MaterialButton>(R.id.btn_get_start).setOnClickListener {
-            navigateToHomeActivity()
-        }
+//        findViewById<TextView>(R.id.textSkip).setOnClickListener {
+//            navigateToHomeActivity()
+////        }
+//        findViewById<MaterialButton>(R.id.btn_get_start).setOnClickListener {
+//            navigateToHomeActivity()
+//        }
     }
     private fun navigateToHomeActivity(){
         startActivity(Intent(applicationContext, EntryActivity::class.java))
+//        finish()
     }
 
     private fun setupIndicators(){
-        indicatorsContainer = findViewById(R.id.indicatorsContainer)
+        indicatorsContainer = binding.indicatorsContainer
         val indicators = arrayOfNulls<ImageView>(onboardingItemsAdapter.itemCount)
         val layoutParams: LinearLayout.LayoutParams =
-            LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
         layoutParams.setMargins(8,0,8,0)
         for(i in indicators.indices){
             indicators[i] = ImageView(applicationContext)
